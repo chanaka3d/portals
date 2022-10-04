30dc44cdb9e3fbefd471b9f128cf24996ba540a7
Chanaka Jayasena <chanakajayasena@chanaka.local>
2022-10-04 11:02:58 +0530

    Fixing few issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css';
 import App from './App';
-import Config from 'Conf/Settings';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
