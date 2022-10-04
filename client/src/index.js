112042cc6ab47500d09aa6f6e2bca1db6c23b17b
dulith <dulithcsenanayake@gmail.com>
2022-10-04 07:31:21 +0530

    Remove SDK & add auth request

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css';
 import App from './App';
-import Settings from 'Settings';
+import Config from 'Conf/Settings';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-    domain={Settings.IDP_DOMAIN}
-    clientId={Settings.IDP_CLIENT_ID}
-    redirectUri={Settings.origin}
-  >
     <App />
-  </Auth0Provider>
   </React.StrictMode>
 );
---
+++ routes/publisher.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+
+const { getCode } = require("../controllers/publisher");
+
+// router.get("/auth", getCode);
+
+module.exports = router;
