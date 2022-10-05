10b57c31bcf76fc092fadad6f25d50a1ee2d2147
dulith <dulithcsenanayake@gmail.com>
2022-10-05 17:50:26 +0530

    Resolve conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -11,9 +11,6 @@ import {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
   <Router>
     <Routes>
       <Route path='/publisher/token' element={<TokenProcessor />} />
