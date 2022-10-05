4284450d93a80ed7eb1e27bcc313c28424012478
dulith <dulithcsenanayake@gmail.com>
2022-10-05 15:46:08 +0530

    Add token request & token exchange request

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TokenProcessor from './TokenProcessor';
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path='/publisher/token' element={<TokenProcessor />} />
+      <Route exact path='/' element={<App />} />
+    </Routes>
+  </Router>
 );
